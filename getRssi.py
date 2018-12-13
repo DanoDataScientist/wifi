a672eb30ef540e641c0fa8ca88492db1cab04e91
Unipig <a.ozeroy@gmail.com>
2018-12-13 14:18:34 +0800

    Collect data for 6 position

--- getRssi.py
+++ getRssi.py
@@ -4,8 +4,8 @@ import time
 import numpy as np
 
 
-rssi = {'TP-LINK_2019': []}
-rssi_m = {'TP-LINK_2019': {}}
+rssi = {'TP-LINK_2019': [], 'MERCURY_4E9E': []}
+rssi_m = {'TP-LINK_2019': {}, 'MERCURY_4E9E': {}}
 
 wifi = PyWiFi()
 for data in wifi.interfaces():
@@ -18,15 +18,21 @@ for i in range(1, 20):
     time.sleep(5)
     results = iface.scan_results()
 
-
     for data in results:
         if data.ssid == 'TP-LINK_2019':
             rssi['TP-LINK_2019'].append(data.signal)
+        if data.ssid == 'MERCURY_4E9E':
+            rssi['MERCURY_4E9E'].append(data.signal)
+print('rssi = ')
 print(rssi)
 
 # calculate mean,std
 rssi_m['TP-LINK_2019']['mean'] = np.mean(rssi['TP-LINK_2019'])
 rssi_m['TP-LINK_2019']['std'] = np.std(rssi['TP-LINK_2019'])
+
+rssi_m['MERCURY_4E9E']['mean'] = np.mean(rssi['MERCURY_4E9E'])
+rssi_m['MERCURY_4E9E']['std'] = np.std(rssi['MERCURY_4E9E'])
+print('rssi_m = ')
 print(rssi_m)
 
 
